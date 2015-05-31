b8f51af281524f8cfa9c1630be1200564d045c9d
flipmotion <flipmotions@gmail.com>
2015-05-31 17:11:56 +0300

    UPD index page
    UPD index page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
+		var owl = $('[data-item="slider"]');
+		
 	$('[data-item="slider"]').owlCarousel({
 		loop:true,
 		margin:0,
 		nav:false,
-		dots:true,
+		dots:false,
 		items:1
+	});
+	$('.navigation-slider .icon-left').click(function() {
+		owl.trigger('prev.owl.carousel');
+	});
+	$('.navigation-slider .icon-right').click(function() {
+		owl.trigger('next.owl.carousel');
 	});
 	$('[data-mask="phone"]').mask("+7 (999) 999-99-99");
 	$('a.smooth').click(function(){
