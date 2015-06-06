8fc65fa1ee23d057e5104d7aaf119a3c6a478b3a
flipmotion <flipmotions@gmail.com>
2015-06-07 01:27:58 +0300

    80% RWD + gallery
    80% RWD + gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-		var owl = $('[data-item="slider"]');
-		
+	var owl = $('[data-item="slider"]');
 	$('[data-item="slider"]').owlCarousel({
 		loop:true,
 		margin:0,
 		nav:false,
 		dots:false,
 		items:1
+	});
+	var to = $('.gallery-main-object img');
+	var tolink = $('.gallery-main-object a');
+	var from = $('.gallery-object img');
+	from.click(function() {
+		var loc = $(this).attr("src");
+		to.attr("src",loc);
+		tolink.attr("href",loc);
 	});
 	$('.navigation-slider .icon-left').click(function() {
 		owl.trigger('prev.owl.carousel');
@@ -15,6 +22,7 @@ $(document).ready(function() {
 		owl.trigger('next.owl.carousel');
 	});
 	$('[data-mask="phone"]').mask("+7 (999) 999-99-99");
+	
 	$('a.smooth').click(function(){
 		$('html, body').animate({
 			scrollTop: $( $.attr(this, 'href') ).offset().top
